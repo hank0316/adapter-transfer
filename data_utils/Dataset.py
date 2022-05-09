977e03d6f1040fd9d0ce8a235e86af9a6a25bafa
hank0316 <b08902126@ntu.edu.tw>
2022-05-09 19:41:50 +0800

    random sample subset

--- data_utils/Dataset.py
+++ data_utils/Dataset.py
@@ -1,11 +1,14 @@
+import torch
+import datasets
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
 class CustomDataset(Dataset):
-    def __init__(self, data_split, task_name=None, tokenizer='roberta-base'):
+    def __init__(self, data_split, task_name=None, tokenizer='roberta-base', train_size=1000):
         self.data = data_split
         self.task_name = task_name
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        self.data['train'] = self.subset(self.data['train'], train_size)
         self.data = self.data.map(self.encode_batch, batched=True)
         self.data = self.data.rename_column('label', 'labels')
         self.data.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
@@ -23,3 +26,7 @@ class CustomDataset(Dataset):
             return self.tokenizer(batch['sentence'], padding='max_length')
         else:
             raise NotImplementedError
+    
+    def subset(self, data_to_subset, size):
+        indices = torch.randperm(len(data_to_subset))[: min(len(data_to_subset), size)]
+        return datasets.Dataset.from_dict(data_to_subset[indices])
