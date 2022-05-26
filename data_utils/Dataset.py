1cdb3b72d7abfe4fc7a58e8a41ea9c99bbb47946
qpoiPeng <jamespeng1209@gmail.com>
2022-05-26 14:35:15 +0800

    Fix Dataset and finetune script

--- data_utils/Dataset.py
+++ data_utils/Dataset.py
@@ -8,11 +8,11 @@ class CustomDataset(Dataset):
         self.data = data_split
         self.task_name = task_name
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        self.seed = seed
         self.data['train'] = self.subset(self.data['train'], train_size)
         self.data = self.data.map(self.encode_batch, batched=True)
         self.data = self.data.rename_column('label', 'labels')
         self.data.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
-        self.seed = seed
 
     def __len__(self):
         return len(self.data)
@@ -29,6 +29,6 @@ class CustomDataset(Dataset):
             raise NotImplementedError
     
     def subset(self, data_to_subset, size):
-        torch.manual_seed(316)
+        torch.manual_seed(self.seed)
         indices = torch.randperm(len(data_to_subset))[: min(len(data_to_subset), size)]
         return datasets.Dataset.from_dict(data_to_subset[indices])
